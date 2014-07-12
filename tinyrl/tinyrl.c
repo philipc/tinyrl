857ddce089351d3e12b904e3d3efe8613269b592
Philip Craig <philipjcraig@gmail.com>
2014-07-12 12:39:06 +1000

    Delete tinyrl/private.h

--- tinyrl/tinyrl.c
+++ tinyrl/tinyrl.c
@@ -7,15 +7,54 @@
 
 /* LIBC HEADERS */
 #include <assert.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
 
 /* POSIX HEADERS */
+#include <termios.h>
 #include <unistd.h>
 
-#include "private.h"
+#include "tinyrl/tinyrl.h"
+#include "tinyrl/vt100.h"
+
+/* define the class member data and virtual methods */
+struct _tinyrl {
+	const char *line;
+	unsigned max_line_length;
+	const char *prompt;
+	size_t prompt_size;
+	char *buffer;
+	size_t buffer_size;
+	bool done;
+	bool completion_over;
+	bool completion_error_over;
+	unsigned point;
+	unsigned end;
+	tinyrl_completion_func_t *attempted_completion_function;
+	int state;
+#define RL_STATE_COMPLETING (0x00000001)
+	char *kill_string;
+#define NUM_HANDLERS 256
+	tinyrl_key_func_t *handlers[NUM_HANDLERS];
+
+	tinyrl_history_t *history;
+	tinyrl_history_iterator_t hist_iter;
+	tinyrl_vt100_t *term;
+	void *context;		/* context supplied by caller
+				 * to tinyrl_readline()
+				 */
+	char echo_char;
+	bool echo_enabled;
+	struct termios default_termios;
+	bool isatty;
+	char *last_buffer;	/* hold record of the previous 
+				   buffer for redisplay purposes */
+	unsigned last_point;	/* hold record of the previous 
+				   cursor position for redisplay purposes */
+};
 
 /*----------------------------------------------------------------------- */
 static void tty_set_raw_mode(tinyrl_t * this)
