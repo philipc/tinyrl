a7795462b5909a250fbfdec034dae24ffe66bba9
Philip Craig <philipjcraig@gmail.com>
2014-07-27 14:15:12 +1000

    example: add completion for space and enter

--- example.c
+++ example.c
@@ -12,7 +12,7 @@ static char **add_match(char **matches, const char *match,
 	return matches;
 }
 
-static bool complete_key(tinyrl_t * t, int key)
+static bool complete(tinyrl_t *t, bool allow_prefix, bool allow_empty)
 {
 	const char *text;
 	unsigned start;
@@ -23,27 +23,52 @@ static bool complete_key(tinyrl_t * t, int key)
 	/* find the start of the current word */
 	text = tinyrl__get_line(t);
 	start = len = tinyrl__get_point(t);
-	while (start && !isspace(text[start - 1])) {
+	while (start && !isspace(text[start - 1]))
 		start--;
-	}
 	text += start;
 	len -= start;
+	if (!len && allow_empty)
+		return true;
 
 	/* build a list of possible completions */
 	matches = NULL;
 	matches = add_match(matches, "exit", text, len);
 	matches = add_match(matches, "help", text, len);
 	matches = add_match(matches, "hello", text, len);
+	matches = add_match(matches, "vi", text, len);
+	matches = add_match(matches, "view", text, len);
 	if (!matches)
 		return false;
 
 	/* select the longest completion */
-	if (tinyrl_complete(t, start, matches, false))
-		ret = tinyrl_insert_text(t, " ");
+	ret = tinyrl_complete(t, start, matches, allow_prefix);
 
 	tinyrl_delete_matches(matches);
 
 	return ret;
+}
+
+static bool tab_key(tinyrl_t *t, int key)
+{
+	if (complete(t, false, false))
+		return tinyrl_insert_text(t, " ");
+	return false;
+}
+
+static bool space_key(tinyrl_t *t, int key)
+{
+	if (complete(t, true, false))
+		return tinyrl_insert_text(t, " ");
+	return false;
+}
+
+static bool enter_key(tinyrl_t *t, int key)
+{
+	if (complete(t, true, true)) {
+		tinyrl_crlf(t);
+		tinyrl_done(t);
+	}
+	return false;
 }
 
 int main(int argc, char *argv[])
@@ -52,7 +77,9 @@ int main(int argc, char *argv[])
 	char *line;
 
 	t = tinyrl_new(stdin, stdout, 0);
-	tinyrl_bind_key(t, '\t', complete_key);
+	tinyrl_bind_key(t, '\t', tab_key);
+	tinyrl_bind_key(t, '\r', enter_key);
+	tinyrl_bind_key(t, ' ', space_key);
 
 	for (;;) {
 		line = tinyrl_readline(t, "> ", NULL);
