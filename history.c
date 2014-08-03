3e1e73a3fd79bf372c2ac942448aefada89b4834
Philip Craig <philipjcraig@gmail.com>
2014-08-03 12:27:55 +1000

    Rename history variables to 'history'

--- history.c
+++ history.c
@@ -21,28 +21,28 @@ struct tinyrl_history {
 /*------------------------------------- */
 struct tinyrl_history *tinyrl_history_new(unsigned limit)
 {
-	struct tinyrl_history *this;
+	struct tinyrl_history *history;
        
-	this = malloc(sizeof(*this));
-	if (!this)
+	history = malloc(sizeof(*history));
+	if (!history)
 		return NULL;
 
-	this->entries = NULL;
-	this->limit = limit;
-	this->length = 0;
-	this->size = 0;
-	return this;
+	history->entries = NULL;
+	history->limit = limit;
+	history->length = 0;
+	history->size = 0;
+	return history;
 }
 
 /*------------------------------------- */
-void tinyrl_history_delete(struct tinyrl_history * this)
+void tinyrl_history_delete(struct tinyrl_history *history)
 {
 	unsigned i;
 
-	for (i = 0; i < this->length; i++)
-		free(this->entries[i]);
-	free(this->entries);
-	free(this);
+	for (i = 0; i < history->length; i++)
+		free(history->entries[i]);
+	free(history->entries);
+	free(history);
 }
 
 /*
@@ -54,18 +54,18 @@ void tinyrl_history_delete(struct tinyrl_history * this)
  * entries vector. Shuffling up the array as necessary 
  */
 static void
-remove_entries(struct tinyrl_history * this, unsigned start, unsigned delta)
+remove_entries(struct tinyrl_history *history, unsigned start, unsigned delta)
 {
 	unsigned end = start + delta;
 	unsigned i;
 
-	assert(end <= this->length);
+	assert(end <= history->length);
 
 	for (i = start; i < end; i++)
-		free(this->entries[i]);
-	memmove(this->entries + start, this->entries + end,
-		sizeof(*this->entries) * (this->length - end));
-	this->length -= delta;
+		free(history->entries[i]);
+	memmove(history->entries + start, history->entries + end,
+		sizeof(*history->entries) * (history->length - end));
+	history->length -= delta;
 }
 
 /*------------------------------------- */
@@ -73,80 +73,80 @@ remove_entries(struct tinyrl_history * this, unsigned start, unsigned delta)
    add an entry to the end of the current array 
    if there is no space returns -1 else 0
    */
-static void append_entry(struct tinyrl_history * this, const char *line)
+static void append_entry(struct tinyrl_history *history, const char *line)
 {
-	if (this->length < this->size) {
-		this->entries[this->length] = strdup(line);
-		this->length++;
+	if (history->length < history->size) {
+		history->entries[history->length] = strdup(line);
+		history->length++;
 	}
 }
 
 /*------------------------------------- */
 /* grow the array if necessary */
-static void grow(struct tinyrl_history * this)
+static void grow(struct tinyrl_history *history)
 {
-	if (this->size == this->length) {
+	if (history->size == history->length) {
 		/* increment the history memory by 10 entries each time we grow */
-		unsigned new_size = this->size + 10;
+		unsigned new_size = history->size + 10;
 		char **new_entries;
 
-		new_entries = realloc(this->entries,
-				      sizeof(*this->entries) * new_size);
+		new_entries = realloc(history->entries,
+				      sizeof(*history->entries) * new_size);
 		if (NULL != new_entries) {
-			this->size = new_size;
-			this->entries = new_entries;
+			history->size = new_size;
+			history->entries = new_entries;
 		}
 	}
 }
 
 /*------------------------------------- */
-void tinyrl_history_add(struct tinyrl_history * this, const char *line)
+void tinyrl_history_add(struct tinyrl_history *history, const char *line)
 {
-	if (this->length && (this->length == this->limit)) {
+	if (history->length && (history->length == history->limit)) {
 		/* remove the oldest entry */
-		remove_entries(this, 0, 1);
+		remove_entries(history, 0, 1);
 	} else {
-		grow(this);
+		grow(history);
 	}
-	append_entry(this, line);
+	append_entry(history, line);
 }
 
 /*------------------------------------- */
-void tinyrl_history_remove(struct tinyrl_history *this, unsigned offset)
+void tinyrl_history_remove(struct tinyrl_history *history, unsigned offset)
 {
-	if (offset < this->length) {
+	if (offset < history->length) {
 		/* do the biz */
-		remove_entries(this, offset, 1);
+		remove_entries(history, offset, 1);
 	}
 }
 
 /*------------------------------------- */
-void tinyrl_history_clear(struct tinyrl_history * this)
+void tinyrl_history_clear(struct tinyrl_history *history)
 {
 	/* free all the entries */
-	remove_entries(this, 0, this->length);
+	remove_entries(history, 0, history->length);
 }
 
 /*------------------------------------- */
-void tinyrl_history_limit(struct tinyrl_history * this, unsigned limit)
+void tinyrl_history_limit(struct tinyrl_history *history, unsigned limit)
 {
-	if (limit && limit < this->length)
-		remove_entries(this, 0, this->length - limit);
-	this->limit = limit;
+	if (limit && limit < history->length)
+		remove_entries(history, 0, history->length - limit);
+	history->limit = limit;
 }
 
 /*
    INFORMATION ABOUT THE HISTORY LIST 
    */
-const char *tinyrl_history_get(const struct tinyrl_history * this,
+const char *tinyrl_history_get(const struct tinyrl_history *history,
 			       unsigned position)
 {
-	if (position < this->length)
-		return this->entries[position];
+	if (position < history->length)
+		return history->entries[position];
 	return NULL;
 }
 
-size_t tinyrl_history_length(const struct tinyrl_history * this)
+size_t tinyrl_history_length(const struct tinyrl_history *history)
 {
-	return this->length;
+	return history->length;
 }
--- history.h
+++ history.h
@@ -17,19 +17,19 @@
 
 extern struct tinyrl_history *tinyrl_history_new(unsigned limit);
 
-extern void tinyrl_history_delete(struct tinyrl_history * instance);
-extern void tinyrl_history_add(struct tinyrl_history * instance, const char *line);
+extern void tinyrl_history_delete(struct tinyrl_history *history);
+extern void tinyrl_history_add(struct tinyrl_history *history, const char *line);
 
 /*
    HISTORY LIST MANAGEMENT 
    */
-extern void tinyrl_history_remove(struct tinyrl_history *instance, unsigned offset);
-extern void tinyrl_history_clear(struct tinyrl_history * instance);
-extern void tinyrl_history_limit(struct tinyrl_history * instance, unsigned limit);
+extern void tinyrl_history_remove(struct tinyrl_history *history, unsigned offset);
+extern void tinyrl_history_clear(struct tinyrl_history *history);
+extern void tinyrl_history_limit(struct tinyrl_history *history, unsigned limit);
 
-extern const char *tinyrl_history_get(const struct tinyrl_history *instance,
+extern const char *tinyrl_history_get(const struct tinyrl_history *history,
 				      unsigned offset);
-extern size_t tinyrl_history_length(const struct tinyrl_history *instance);
+extern size_t tinyrl_history_length(const struct tinyrl_history *history);
 
 #endif				/* _tinyrl_history_h */
 /** @} tinyrl_history */
